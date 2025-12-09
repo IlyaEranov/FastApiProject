de1830c2834edb67f81b6850090c13876207941f
IlyaEranov <ilya.erannov123454321@gmail.com>
2025-12-09 23:47:50 +0500

    pr8

--- models/events.py
+++ models/events.py
@@ -1,14 +1,13 @@
-from pydantic import BaseModel
-from typing import List, Optional
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: Optional[int] = None
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
-    image: str
+    image: str 
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
-
     model_config = {
         "json_schema_extra": {
             "example": {
@@ -20,3 +19,22 @@ class Event(BaseModel):
             }
         }
     }
+
+class EventUpdate(SQLModel): 
+    title: Optional[str] 
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+    }
+

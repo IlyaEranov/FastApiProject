fc8380bb7ab95b533652efd819a7f8d7be74b9f6
IlyaEranov <ilya.erannov123454321@gmail.com>
2025-12-09 23:18:46 +0500

    pr7

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from todo import todo_router
+from routes.users import user_router
+from routes.events import event_router
+import uvicorn
 
 app = FastAPI()
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
-@app.get("/")
-async def welcome() -> dict:
-    return { "message": "Hello World"}
-
-app.include_router(todo_router)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
